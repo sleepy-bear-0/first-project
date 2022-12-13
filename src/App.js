5174b1eebff89cef187663a4a56cb98b067f0f54
Eetu Turtiainen <the.sleepybear0@gmail.com>
2022-12-13 11:44:13 +0200

    AddingContainers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import {Cta, Brand, Navbar} from './components';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+
+
+    </div>
   )
 };
 
